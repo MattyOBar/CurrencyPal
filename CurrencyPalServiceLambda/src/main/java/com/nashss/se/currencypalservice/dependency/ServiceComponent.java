695ed2a94347a0a42dd9969fa74f825046e39fb4
mason snyder <msnyder193@gmail.com>
2022-11-10 14:23:05 -0600

    setting up empty boilerplate template

---
+++ CurrencyPalServiceLambda/src/main/java/com/nashss/se/currencypalservice/dependency/ServiceComponent.java
@@ -0,0 +1,44 @@
+package com.nashss.se.musicplaylistservice.dependency;
+
+import dagger.Component;
+
+import javax.inject.Singleton;
+
+/**
+ * Dagger component for providing dependency injection in the Music Playlist Service.
+ */
+@Singleton
+@Component(modules = {DaoModule.class, MetricsModule.class})
+public interface ServiceComponent {
+
+    /**
+     * Provides the relevant activity.
+     * @return AddSongToPlaylistActivity
+     */
+    AddSongToPlaylistActivity provideAddSongToPlaylistActivity();
+
+    /**
+     * Provides the relevant activity.
+     * @return CreatePlaylistActivity
+     */
+    CreatePlaylistActivity provideCreatePlaylistActivity();
+
+    /**
+     * Provides the relevant activity.
+     * @return GetPlaylistActivity
+     */
+    GetPlaylistActivity provideGetPlaylistActivity();
+
+    /**
+     * Provides the relevant activity.
+     * @return GetPlaylistSongsActivity
+     */
+    GetPlaylistSongsActivity provideGetPlaylistSongsActivity();
+
+    /**
+     * Provides the relevant activity.
+     * @return UpdatePlaylistActivity
+     */
+    UpdatePlaylistActivity provideUpdatePlaylistActivity();
+
+}
---
+++ CurrencyPalServiceLambda/src/main/java/com/nashss/se/currencypalservice/models/CurrencyModel.java
@@ -0,0 +1,4 @@
+package com.nashss.se.currencypalservice.models;
+
+public class CurrencyModel {
+}
