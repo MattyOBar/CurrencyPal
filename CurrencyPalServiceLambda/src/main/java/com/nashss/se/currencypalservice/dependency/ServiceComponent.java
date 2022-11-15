cc63a40f10e5c10895ca17846b4a58622316ba66
Matthew OBar <matthewobar@gmail.com>
2022-11-15 15:03:10 -0600

    implemented getCurrecyLambda

--- CurrencyPalServiceLambda/src/main/java/com/nashss/se/currencypalservice/dependency/ServiceComponent.java
+++ CurrencyPalServiceLambda/src/main/java/com/nashss/se/currencypalservice/dependency/ServiceComponent.java
@@ -1,5 +1,7 @@
 package com.nashss.se.currencypalservice.dependency;
 
+import com.nashss.se.currencypalservice.activity.CreateCustomerActivity;
+import com.nashss.se.currencypalservice.activity.GetCurrencyActivity;
 import dagger.Component;
 
 import javax.inject.Singleton;
@@ -10,35 +12,9 @@ import javax.inject.Singleton;
 @Singleton
 @Component(modules = {DaoModule.class, MetricsModule.class})
 public interface ServiceComponent {
-
-//    /**
-//     * Provides the relevant activity.
-//     * @return AddSongToPlaylistActivity
-//     */
-//    AddSongToPlaylistActivity provideAddSongToPlaylistActivity();
-//
-//    /**
-//     * Provides the relevant activity.
-//     * @return CreatePlaylistActivity
-//     */
-//    CreatePlaylistActivity provideCreatePlaylistActivity();
-//
-//    /**
-//     * Provides the relevant activity.
-//     * @return GetPlaylistActivity
-//     */
-//    GetPlaylistActivity provideGetPlaylistActivity();
-//
-//    /**
-//     * Provides the relevant activity.
-//     * @return GetPlaylistSongsActivity
-//     */
-//    GetPlaylistSongsActivity provideGetPlaylistSongsActivity();
-//
-//    /**
-//     * Provides the relevant activity.
-//     * @return UpdatePlaylistActivity
-//     */
-//    UpdatePlaylistActivity provideUpdatePlaylistActivity();
-
+    /**
+     * Provides the relevant activity.
+     * @return GetCurrencyActivity
+     */
+    GetCurrencyActivity provideGetCurrencyActivity();
 }
