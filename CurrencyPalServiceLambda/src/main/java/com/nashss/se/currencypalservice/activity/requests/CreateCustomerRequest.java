9d91096efc67a8e2058165f2cc131b472d96cbcc
alexleuang <asleuang@gmail.com>
2022-11-17 13:28:09 -0600

    Customer Request

--- CurrencyPalServiceLambda/src/main/java/com/nashss/se/currencypalservice/activity/requests/CreateCustomerRequest.java
+++ CurrencyPalServiceLambda/src/main/java/com/nashss/se/currencypalservice/activity/requests/CreateCustomerRequest.java
@@ -1,4 +1,61 @@
 package com.nashss.se.currencypalservice.activity.requests;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+
+@JsonDeserialize
 public class CreateCustomerRequest {
+    private final String customerId;
+    private final String name;
+    private final String dateOfBirth;
+    public CreateCustomerRequest(String customerId, String name, String dateOfBirth) {
+        this.customerId = customerId;
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+    @Override
+    public String toString() {
+        return "CreateCustomerRequest{" +
+                "customerId =" + customerId +
+                "name =" +  name +
+                "dateOfBirth =" + dateOfBirth;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    @JsonPOJOBuilder
+    public static class Builder {
+        private String customerId;
+        private String name;
+        private String dateOfBirth;
+
+        public Builder withCustomerId(String customerId) {
+            this.customerId = customerId;
+            return this;
+        }
+
+        public Builder withName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder withDateOfBirth(String dateOfBirth) {
+            this.dateOfBirth = dateOfBirth;
+            return this;
+        }
+    }
 }
--- CurrencyPalServiceLambda/src/main/java/com/nashss/se/currencypalservice/activity/requests/GetCustomerRequest.java
+++ CurrencyPalServiceLambda/src/main/java/com/nashss/se/currencypalservice/activity/requests/GetCustomerRequest.java
@@ -1,4 +1,37 @@
 package com.nashss.se.currencypalservice.activity.requests;
 
 public class GetCustomerRequest {
+    private final String customerId;
+
+
+    public GetCustomerRequest(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public String toString() {
+        return "GetCustomerRequest:" +
+                "customerId=" + customerId;
+    }
+
+    //CHECKSTYLE:OFF:Builder
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private String customerId;
+
+        public Builder withCustomerId(String customerId) {
+            this.customerId = customerId;
+            return this;
+        }
+
+        public GetCustomerRequest build() {
+            return new GetCustomerRequest(customerId);
+        }
+    }
 }
