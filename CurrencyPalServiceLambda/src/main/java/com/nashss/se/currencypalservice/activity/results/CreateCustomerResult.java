690533447ae5ecc28bfa3e48d93feddb4d2453c3
alexleuang <asleuang@gmail.com>
2022-11-17 14:03:03 -0600

    Customer Results

--- CurrencyPalServiceLambda/src/main/java/com/nashss/se/currencypalservice/activity/results/CreateCustomerResult.java
+++ CurrencyPalServiceLambda/src/main/java/com/nashss/se/currencypalservice/activity/results/CreateCustomerResult.java
@@ -1,4 +1,38 @@
 package com.nashss.se.currencypalservice.activity.results;
 
+import com.nashss.se.currencypalservice.dynamodb.models.Customer;
+import com.nashss.se.currencypalservice.models.CustomerModel;
+
 public class CreateCustomerResult {
+    private final CustomerModel customer;
+
+    private CreateCustomerResult(CustomerModel customer) {
+        this.customer = customer;
+    }
+
+    public CustomerModel getCustomer() {
+        return customer;
+    }
+
+    public String toString() {
+        return "CreateCustomerResult" +
+                "customer: " + customer;
+    }
+    //CHECKSTYLE:OFF:Builder
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private CustomerModel customer;
+
+        public Builder withPlaylist(CustomerModel customer) {
+            this.customer = customer;
+            return this;
+        }
+
+        public CreateCustomerResult build() {
+            return new CreateCustomerResult(customer);
+        }
+    }
 }
