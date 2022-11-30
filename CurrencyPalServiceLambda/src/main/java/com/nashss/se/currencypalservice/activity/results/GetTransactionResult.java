779d25b67703c76aa846676d5fe9fe470d99f631
Mason Snyder <msnyder193@gmail.com>
2022-11-30 14:05:51 -0600

    transaction request result activity model and modelconverter

--- CurrencyPalServiceLambda/src/main/java/com/nashss/se/currencypalservice/activity/results/GetTransactionResult.java
+++ CurrencyPalServiceLambda/src/main/java/com/nashss/se/currencypalservice/activity/results/GetTransactionResult.java
@@ -1,4 +1,33 @@
 package com.nashss.se.currencypalservice.activity.results;
 
+import com.nashss.se.currencypalservice.models.TransactionModel;
+
 public class GetTransactionResult {
+    private final TransactionModel transactionModel;
+
+    private GetTransactionResult(TransactionModel transactionModel) {
+        this.transactionModel = transactionModel;
+    }
+
+    public TransactionModel getTransactionModel() {
+        return transactionModel;
+    }
+
+    //CHECKSTYLE:OFF:Builder
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private TransactionModel transactionModel;
+
+        public Builder withTransaction(TransactionModel transactionModel) {
+            this.transactionModel = transactionModel;
+            return this;
+        }
+
+        public GetTransactionResult build() {
+            return new GetTransactionResult(transactionModel);
+        }
+    }
 }
