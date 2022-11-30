5faf56967917177d9a01bc0d6bf6c726f48279a4
Mason Snyder <msnyder193@gmail.com>
2022-11-30 10:23:34 -0600

    transaction and dao setup

--- CurrencyPalServiceLambda/src/main/java/com/nashss/se/currencypalservice/dynamodb/models/Transaction.java
+++ CurrencyPalServiceLambda/src/main/java/com/nashss/se/currencypalservice/dynamodb/models/Transaction.java
@@ -1,4 +1,104 @@
 package com.nashss.se.currencypalservice.dynamodb.models;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+
+import java.util.Objects;
+
+@DynamoDBTable(tableName = "Transactions")
 public class Transaction {
+
+    private String transactionId;
+    private String customerName;
+    private String startCurrency;
+    private String endCurrency;
+    private double startAmount;
+    private double endAmount;
+
+    public Transaction(String transactionId, String customerName, String startCurrency, String endCurrency, double startAmount, double endAmount){
+        this.transactionId = transactionId;
+        this.customerName = customerName;
+        this.startCurrency = startCurrency;
+        this.endCurrency = endCurrency;
+        this.startAmount = startAmount;
+        this.endAmount = endAmount;
+    }
+
+    @DynamoDBHashKey(attributeName = "transactionId")
+    public String getTransactionId(){
+        return transactionId;
+    }
+
+    @DynamoDBAttribute(attributeName = "customerName")
+    public String getCustomerName(){
+        return customerName;
+    }
+
+
+    @DynamoDBAttribute(attributeName = "startingCurrency")
+    public String  getStartCurrency(){
+        return startCurrency;
+    }
+
+    @DynamoDBAttribute(attributeName = "endCurrency")
+    public String getEndCurrency(){
+        return endCurrency;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public void setStartCurrency(String startCurrency) {
+        this.startCurrency = startCurrency;
+    }
+
+    public void setEndCurrency(String endCurrency) {
+        this.endCurrency = endCurrency;
+    }
+
+    public void setStartAmount(double startAmount) {
+        this.startAmount = startAmount;
+    }
+
+    public void setEndAmount(double endAmount) {
+        this.endAmount = endAmount;
+    }
+
+    @DynamoDBAttribute(attributeName = "startAmount")
+    public double getStartAmount(){
+        return startAmount;
+    }
+
+    @DynamoDBAttribute(attributeName = "endAmount")
+    public double getEndAmount(){
+        return endAmount;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "transactionId='" + transactionId + '\'' +
+                ", customerName='" + customerName + '\'' +
+                ", startCurrency='" + startCurrency + '\'' +
+                ", endCurrency='" + endCurrency + '\'' +
+                ", startAmount=" + startAmount +
+                ", endAmount=" + endAmount +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Transaction that = (Transaction) o;
+        return Double.compare(that.startAmount, startAmount) == 0 && Double.compare(that.endAmount, endAmount) == 0 && Objects.equals(transactionId, that.transactionId) && Objects.equals(startUser, that.startUser) && Objects.equals(endUser, that.endUser) && Objects.equals(startCurrency, that.startCurrency) && Objects.equals(endCurrency, that.endCurrency);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(transactionId, customerName, startCurrency, endCurrency, startAmount, endAmount);
+    }
+
 }
--- CurrencyPalServiceLambda/src/main/java/com/nashss/se/currencypalservice/models/TransactionModel.java
+++ CurrencyPalServiceLambda/src/main/java/com/nashss/se/currencypalservice/models/TransactionModel.java
@@ -1,4 +1,19 @@
 package com.nashss.se.currencypalservice.models;
 
 public class TransactionModel {
+    private final String transactionId;
+    private final String customerName;
+    private final String startCurrency;
+    private final String endCurrency;
+    private final double startAmount;
+    private final double endAmount;
+
+    public TransactionModel(String transactionId, String customerName, String startCurrency, String endCurrency, double startAmount, double endAmount) {
+        this.transactionId = transactionId;
+        this.customerName = customerName;
+        this.startCurrency = startCurrency;
+        this.endCurrency = endCurrency;
+        this.startAmount = startAmount;
+        this.endAmount = endAmount;
+    }
 }
